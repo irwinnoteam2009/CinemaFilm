c2fcb8e5780ae9c5a996e02e839f8cee81fbf30e
HellB0ys <irwin.noteam.2009@gmail.com>
2018-07-02 22:44:37 +0300

    fix MainActivity creation of MovieListFragment, fix MovieListFragment/MovieListAdapter callback

--- app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/main/MainActivity.kt
+++ app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/main/MainActivity.kt
@@ -11,32 +11,28 @@ import ru.meteoctx.hellboys.cinemafilm.presentation.movie.list.MovieListFragment
 class MainActivity: MvpAppCompatActivity(), MainView {
 
     @InjectPresenter lateinit var presenter: MainPresenter
-    private lateinit var moviesFragment: MovieListFragment
+    private var moviesFragment: MovieListFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         if (savedInstanceState == null) {
-            moviesFragment = MovieListFragment.newInstance {
-                val infoFragment = MovieInfoFragment.newInstance(0, it)
-                supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, infoFragment)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .addToBackStack(null)
-                        .commit()
-            }
-
+        moviesFragment = supportFragmentManager.findFragmentByTag(MovieListFragment.TAG) as MovieListFragment?
+        if (moviesFragment == null) {
+            moviesFragment = MovieListFragment()
             supportFragmentManager.beginTransaction()
                     .add(R.id.container, moviesFragment, MovieListFragment.TAG)
                     .commit()
-        } else
-             moviesFragment = supportFragmentManager.findFragmentByTag(MovieListFragment.TAG) as MovieListFragment
+        }
 
-
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
+       moviesFragment?.setMovieSelectListener {
+            val infoFragment = MovieInfoFragment.newInstance(it)
+            supportFragmentManager.beginTransaction()
+                    .hide(moviesFragment)
+                    .add(R.id.container, infoFragment)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                    .addToBackStack(null)
+                    .commit()
+        }
     }
 }
