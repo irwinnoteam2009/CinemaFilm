41f25d35a4d8ea66c6d2210693cee655a8983e56
HellB0ys <irwin.noteam.2009@gmail.com>
2018-07-02 22:43:26 +0300

    add debug information to MovieInfoPresenter, rewrite onViewCreated for MovieInfoFragment to loading movie from bundle

--- app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/movie/info/MovieInfoPresenter.kt
+++ app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/movie/info/MovieInfoPresenter.kt
@@ -1,8 +1,31 @@
 package ru.meteoctx.hellboys.cinemafilm.presentation.movie.info
 
+import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 
 @InjectViewState
 class MovieInfoPresenter: MvpPresenter<MovieInfoView>() {
+
+    private val TAG = "MovieInfo"
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        Log.wtf(TAG, "first attach")
+    }
+
+    override fun attachView(view: MovieInfoView?) {
+        super.attachView(view)
+        Log.wtf(TAG, "attach")
+    }
+
+    override fun detachView(view: MovieInfoView?) {
+        super.detachView(view)
+        Log.wtf(TAG, "detach")
+    }
+
+    override fun onDestroy() {
+        Log.wtf(TAG, "destroy")
+        super.onDestroy()
+    }
 }
