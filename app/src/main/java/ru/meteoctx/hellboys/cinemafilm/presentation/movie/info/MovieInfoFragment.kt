b451ea2299c5585fbb742f7d9c7471950f3b547f
HellB0ys <irwin.noteam.2009@gmail.com>
2018-07-04 02:47:05 +0300

    add exoplayer logic: init, release, restore position on rotation

--- app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/movie/info/MovieInfoFragment.kt
+++ app/src/main/java/ru/meteoctx/hellboys/cinemafilm/presentation/movie/info/MovieInfoFragment.kt
@@ -1,5 +1,6 @@
 package ru.meteoctx.hellboys.cinemafilm.presentation.movie.info
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,13 @@ import android.view.ViewGroup
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.bumptech.glide.Glide
+import com.google.android.exoplayer2.*
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.upstream.*
+import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.fragment_movie_info.*
 import ru.meteoctx.hellboys.cinemafilm.R
 import ru.meteoctx.hellboys.cinemafilm.domain.model.Movie
@@ -30,6 +38,7 @@ class MovieInfoFragment: MvpAppCompatFragment(), MovieInfoView {
 
     @InjectPresenter lateinit var presenter: MovieInfoPresenter
     private val adapter: InfoAdapter = InfoAdapter()
+    private var player: SimpleExoPlayer? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_movie_info, container, false)
@@ -50,5 +59,38 @@ class MovieInfoFragment: MvpAppCompatFragment(), MovieInfoView {
         adapter.setField("Release", movie.releaseAt ?: "")
         adapter.setField("Popularity", movie.popularity?.toString() ?: "0")
         adapter.setField("Vote", "${movie.voteAverage?.toString()} (${movie.voteCount?.toString()})" ?: "0 (0)")
+
+        val position: Long = savedInstanceState?.getLong("position") ?: 0
+        initPlayer(movie.video, position)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putLong("position", player?.currentPosition ?: 0)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onPause() {
+        player?.release()
+        super.onPause()
+    }
+
+    private fun initPlayer(url: String?, position: Long) {
+        url ?: return
+
+        val bandwidthMeter: BandwidthMeter = DefaultBandwidthMeter()
+        val videoTrackSelectionFactory = AdaptiveTrackSelection.Factory(bandwidthMeter)
+        val trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+        player  = ExoPlayerFactory.newSimpleInstance(context, trackSelector)
+        player_view.player = player
+        player?.playWhenReady = true
+
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                context,
+                Util.getUserAgent(context, "cinemaFilm"))
+        val videoSource: MediaSource = ExtractorMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(Uri.parse(url))
+
+        player?.prepare(videoSource)
+        player?.seekTo(position)
     }
 }
