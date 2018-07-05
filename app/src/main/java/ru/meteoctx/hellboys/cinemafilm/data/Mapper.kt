9ee3e0c026f7feaf8ea7f1e0a6f97eb937ac6c07
HellB0ys <irwin.noteam.2009@gmail.com>
2018-07-05 23:51:01 +0300

    fix mapper for mapping arrays with NULL items (from json unmarshall)

--- app/src/main/java/ru/meteoctx/hellboys/cinemafilm/data/Mapper.kt
+++ app/src/main/java/ru/meteoctx/hellboys/cinemafilm/data/Mapper.kt
@@ -3,9 +3,9 @@ package ru.meteoctx.hellboys.cinemafilm.data
 interface Mapper<From, To> {
     // from data to domain
     fun mapTo(from: From): To
-    fun mapTo(elements: List<From>?): List<To> = elements?.map { item -> mapTo(item) } ?: emptyList()
+    fun mapTo(elements: List<From?>?): List<To> = elements?.filter { it != null }?.map { item -> mapTo(item!!) } ?: emptyList()
 
     // from domain to data
     fun mapFrom(entity: To): From
-    fun mapFrom(elements: List<To>?): List<From> = elements?.map { item -> mapFrom(item) } ?: emptyList()
+    fun mapFrom(elements: List<To?>?): List<From> = elements?.filter { it != null }?.map { item -> mapFrom(item!!) } ?: emptyList()
 }
